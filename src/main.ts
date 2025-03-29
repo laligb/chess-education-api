34f1ce182a68772a2027938b907a1649b73be50c
Lali Bibilashvili <lalibibilashvili@gmail.com>
2025-03-29 18:09:25 +0100

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = ['http://localhost:3001', 'https://lbchess.com'];
+
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
